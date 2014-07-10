6b0052d13dec85558b24a81eb51778268322d7df
David J. Kennedy <dkenn2@pdx.edu>
2014-07-09 18:54:10 -0700

    Added test for 2.00.  Reformatted to work cross computers.

--- CheckKata/src/main/java/edu/pdx/cs410J/whitlock/NumberToWords.java
+++ CheckKata/src/main/java/edu/pdx/cs410J/whitlock/NumberToWords.java
@@ -1,7 +1,22 @@
 package edu.pdx.cs410J.whitlock;
 
 public class NumberToWords {
-  public static String numberToWords(double number) {
-    return "one dollar";
-  }
+	public static String numberToWords(double number) {
+		return convertNumberToWords(number) + " " + getDollarWord(number);
+	}
+
+	private static String getDollarWord(double number) {
+		if (number == 1.00) {
+			return "dollar";
+		} else {
+			return "dollars";
+		}
+	}
+	private static String convertNumberToWords(double number) {
+		if (number == 1.00) {
+			return "one";
+		} else {
+			return "two";
+		}
+	}
 }
--- CheckKata/src/test/java/edu/pdx/cs410J/whitlock/NumberToWordsTest.java
+++ CheckKata/src/test/java/edu/pdx/cs410J/whitlock/NumberToWordsTest.java
@@ -11,5 +11,9 @@ public class NumberToWordsTest {
   public void number1IsOne() {
     assertThat(NumberToWords.numberToWords(1.00), equalTo("one dollar"));
   }
+  @Test
+  public void number2IsTwo(){
+	  assertThat(NumberToWords.numberToWords(2.00),equalTo("two dollars"));
+  }
 
 }
