66a3cdc3f9b42d16122e834ceca68338ef92e153
David Whitlock <notmyrealaddress@email.com>
2014-07-16 19:30:09 -0700

    Got first test to pass.

---
+++ RomanNumeralsKata/src/main/java/edu/pdx/cs410J/whitlock/RomanNumerals.java
@@ -0,0 +1,10 @@
+package edu.pdx.cs410J.whitlock;
+
+/**
+ * This class is represents a <code>RomanNumerals</code>.
+ */                                                                                 
+public class RomanNumerals {
+  public static String romanNumeralFor(int number) {
+    return "I";
+  }
+}
---
+++ RomanNumeralsKata/src/test/java/edu/pdx/cs410J/whitlock/RomanNumeralsTest.java
@@ -0,0 +1,21 @@
+package edu.pdx.cs410J.whitlock;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * JUnit tests for the RomanNumerals class.  These tests extend <code>InvokeMainTestCase</code>
+ * which allows them to easily invoke the <code>main</code> method of <code>RomanNumerals</code>.
+ * They also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>matchers
+ * for more readable assertion statements.
+ */
+public class RomanNumeralsTest {
+
+  @Test
+  public void romanNumeralFor1IsI() {
+    assertThat(RomanNumerals.romanNumeralFor(1), equalTo("I"));
+  }
+
+}
