e987adc0a33d677f10abfe3f70539f1df0b07615
David Whitlock <notmyrealaddress@email.com>
2014-08-05 19:19:24 -0700

    Added files created from Maven project archetype.

---
+++ PacManKataGWT/src/main/java/edu/pdx/cs410J/whitlock/server/PingServiceImpl.java
@@ -0,0 +1,20 @@
+package edu.pdx.cs410J.whitlock.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.whitlock.client.Airline;
+import edu.pdx.cs410J.whitlock.client.Flight;
+import edu.pdx.cs410J.whitlock.client.PingService;
+
+/**
+ * The server-side implementation of the Airline service
+ */
+public class PingServiceImpl extends RemoteServiceServlet implements PingService
+{
+    public AbstractAirline ping()
+    {
+        Airline airline = new Airline();
+        airline.addFlight( new Flight() );
+        return airline;
+    }
+}
