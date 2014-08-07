8fcdb55ded9cd48b7ad68a5ccee2c74649037be3
David Whitlock <notmyrealaddress@email.com>
2014-08-06 18:31:45 -0700

    Initially have a 5x5 Level.

--- PacManKataGWT/src/main/java/edu/pdx/cs410J/whitlock/client/LevelEditor.java
+++ PacManKataGWT/src/main/java/edu/pdx/cs410J/whitlock/client/LevelEditor.java
@@ -5,23 +5,34 @@ import com.google.gwt.user.client.ui.*;
 public class LevelEditor extends DockPanel {
 
   public LevelEditor() {
-    add(createRowAndColumnPanel(), NORTH);
+    int initialNumberOfRows = 5;
+    int initialNumberOfColumns = 5;
+    add(createRowAndColumnPanel(initialNumberOfRows, initialNumberOfColumns), NORTH);
+    add(createTextArea(initialNumberOfRows, initialNumberOfColumns), CENTER);
   }
 
-  private IsWidget createRowAndColumnPanel() {
+  private IsWidget createTextArea(int initialNumberOfRows, int initialNumberOfColumns) {
+    TextArea textArea = new TextArea();
+    textArea.setVisibleLines(initialNumberOfRows);
+    textArea.setCharacterWidth(initialNumberOfColumns);
+    return textArea;
+  }
+
+  private IsWidget createRowAndColumnPanel(int initialNumberOfRows, int initialNumberOfColumns) {
 
     VerticalPanel panel = new VerticalPanel();
-    createTextBoxWithLabel("Number of Rows", panel);
-    createTextBoxWithLabel("Number of Columns", panel);
+    createTextBoxWithLabel("Number of Rows", initialNumberOfRows, panel);
+    createTextBoxWithLabel("Number of Columns", initialNumberOfColumns, panel);
 
     return panel;
   }
 
-  private TextBox createTextBoxWithLabel(String labelString, VerticalPanel parent) {
+  private TextBox createTextBoxWithLabel(String labelString, int initialValue, VerticalPanel parent) {
     Label label = new Label(labelString);
     TextBox textBox = new TextBox();
     textBox.setVisibleLength(3);
     textBox.setMaxLength(3);
+    textBox.setText(String.valueOf(initialValue));
 
     HorizontalPanel panel = new HorizontalPanel();
     panel.add(label);
