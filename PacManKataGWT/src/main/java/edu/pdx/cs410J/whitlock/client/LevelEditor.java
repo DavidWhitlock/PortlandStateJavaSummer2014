e6fc46989abd597f2951d5e4936acc0acb708132
David Whitlock <notmyrealaddress@email.com>
2014-08-06 18:49:09 -0700

    Update the size of the text field when the number of rows/columns text box loses keyboard focus.

--- PacManKataGWT/src/main/java/edu/pdx/cs410J/whitlock/client/LevelEditor.java
+++ PacManKataGWT/src/main/java/edu/pdx/cs410J/whitlock/client/LevelEditor.java
@@ -1,8 +1,15 @@
 package edu.pdx.cs410J.whitlock.client;
 
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 
 public class LevelEditor extends DockPanel {
+
+  private TextBox numberOfRowsTextBox;
+  private TextBox numberOfColumnsTextBox;
+  private TextArea textArea;
 
   public LevelEditor() {
     int initialNumberOfRows = 5;
@@ -12,27 +19,33 @@ public class LevelEditor extends DockPanel {
   }
 
   private IsWidget createTextArea(int initialNumberOfRows, int initialNumberOfColumns) {
-    TextArea textArea = new TextArea();
-    textArea.setVisibleLines(initialNumberOfRows);
-    textArea.setCharacterWidth(initialNumberOfColumns);
+    textArea = new TextArea();
+    setTextAreaSize(initialNumberOfRows, initialNumberOfColumns);
     return textArea;
   }
 
   private IsWidget createRowAndColumnPanel(int initialNumberOfRows, int initialNumberOfColumns) {
 
     VerticalPanel panel = new VerticalPanel();
-    createTextBoxWithLabel("Number of Rows", initialNumberOfRows, panel);
-    createTextBoxWithLabel("Number of Columns", initialNumberOfColumns, panel);
+    numberOfRowsTextBox = createTextBoxWithLabel("Number of Rows", initialNumberOfRows, panel);
+    numberOfColumnsTextBox = createTextBoxWithLabel("Number of Columns", initialNumberOfColumns, panel);
 
     return panel;
   }
 
   private TextBox createTextBoxWithLabel(String labelString, int initialValue, VerticalPanel parent) {
     Label label = new Label(labelString);
+
     TextBox textBox = new TextBox();
     textBox.setVisibleLength(3);
     textBox.setMaxLength(3);
     textBox.setText(String.valueOf(initialValue));
+    textBox.addBlurHandler(new BlurHandler() {
+      @Override
+      public void onBlur(BlurEvent blurEvent) {
+        updateSizeOfTextArea();
+      }
+    });
 
     HorizontalPanel panel = new HorizontalPanel();
     panel.add(label);
@@ -40,6 +53,30 @@ public class LevelEditor extends DockPanel {
     parent.add(panel);
 
     return textBox;
+  }
+
+  private void updateSizeOfTextArea() {
+    int numberOfRows = getIntValueOfTextBox(numberOfRowsTextBox);
+    int numberOfColumns = getIntValueOfTextBox(numberOfColumnsTextBox);
+
+    setTextAreaSize(numberOfRows, numberOfColumns);
+  }
+
+  private void setTextAreaSize(int numberOfRows, int numberOfColumns) {
+    textArea.setVisibleLines(numberOfRows);
+    textArea.setCharacterWidth(numberOfColumns);
+  }
+
+  private int getIntValueOfTextBox(TextBox textBox) {
+    String value = textBox.getValue();
+    try {
+      return Integer.parseInt(value);
+
+    } catch (NumberFormatException ex) {
+      Window.alert("Value \"" + value + "\" is not a valid number");
+      textBox.setFocus(true);
+      throw ex;
+    }
   }
 
 }
